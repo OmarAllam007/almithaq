75adeeff25e67f1e818c204adef1c10786d8f8fc
omargarana <omar.ceh007@gmail.com>
2026-06-09 21:29:14 +0300

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 @include('_partials._header')
-    @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
+    {{-- @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"]) --}}
+    @vite('resources/js/app.ts')
+
     @inertiaHead
     @if(config('services.google_adsense.client'))
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client={{ config('services.google_adsense.client') }}" crossorigin="anonymous"></script>
