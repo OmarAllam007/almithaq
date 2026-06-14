12ab585f113a10352c4773f80b4db0584fadf6c9
omargarana <omar.ceh007@gmail.com>
2026-06-14 18:00:40 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ use App\Http\Controllers\UserInteractionsController;
 use App\Http\Controllers\VerificationController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
 // Auth::loginUsingId(1);
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
